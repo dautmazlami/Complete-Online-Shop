fa316613000367a6c54e8cb5acfda648132554d3
dautmazlami <daut.mazlami@gmail.com>
2023-03-02 20:01:19 +0100

    Added image upload functionallity

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -2,12 +2,15 @@ function getProducts(req, res) {
     res.render('admin/products/all-products');
 }
 
-function getNewProduct() {
-    
+function getNewProduct(req, res) {
+    res.render('admin/products/new-product');
 }
 
-function createNewProduct() {
-    
+function createNewProduct(req, res) {
+    console.log(req.body);
+    console.log(req.file);
+
+    res.redirect('/admin/products');
 }
 
 module.exports = {
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin.controllers");
+const imageUploadMiddleware = require('../middlewares/image-upload');
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.get("/products", adminController.getProducts);
 // this with handle /admin/products because we definet in the routes in the app.js like this --> app.use('/admin',adminRoutes);
 
 router.get("/products/new", adminController.getNewProduct);
+
+router.post('/products', imageUploadMiddleware, adminController.createNewProduct);
 
 module.exports = router;
