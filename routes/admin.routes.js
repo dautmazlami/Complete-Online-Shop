5714693afb4089f7459393bd67f3fd8135c43902
Daut Mazlami <daut.mazlami@gmail.com>
2023-03-09 15:50:46 +0100

    Making product editable

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -6,10 +6,14 @@ const imageUploadMiddleware = require('../middlewares/image-upload');
 const router = express.Router();
 
 router.get("/products", adminController.getProducts);
-// this with handle /admin/products because we definet in the routes in the app.js like this --> app.use('/admin',adminRoutes);
+// this with handle /admin/products because we define in the routes in the app.js like this --> app.use('/admin',adminRoutes);
 
 router.get("/products/new", adminController.getNewProduct);
 
 router.post('/products', imageUploadMiddleware, adminController.createNewProduct);
+
+router.get('/products/:id', adminController.getUpdateProduct);
+
+router.post('/products/:id', adminController.updateProduct)
 
 module.exports = router;
